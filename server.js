c7249ab105b1a92c7ca7f7ea44bcaa38d4393908
tony-tripulca <tony.tripulca@gmail.com>
2020-07-13 23:10:40 +0800

    Front end can now get/send request to back end

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var body_parser = require('body-parser');
 var app = express();
 
 app.use(express.static('./public'));
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended: false}));
 
 var messages = [
     { name: 'Tim', message: 'Hi' },
@@ -10,6 +13,11 @@ var messages = [
 
 app.get('/messages', (req, res) => {
     res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
 });
 
 var server = app.listen(3000, () => {
