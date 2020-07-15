6d8e7d94f2ee3572b0940f465d105ec6a19f5083
tony-tripulca <tony.tripulca@gmail.com>
2020-07-15 23:59:08 +0800

    Setup mongodb connection and save message to db

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var body_parser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static('./public'));
 app.use(body_parser.json());
-app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.urlencoded({ extended: false }));
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String,
+}, 'chats');
 
 var messages = [
     { name: 'Tim', message: 'Hi' },
@@ -18,14 +24,28 @@ app.get('/messages', (req, res) => {
 });
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    io.emit('message', req.body);
-    res.sendStatus(200);
+    var message = new Message(req.body);
+
+    message.save((err) => {
+        if (err) sendStatus(500);
+
+        messages.push(req.body);
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    });
 });
 
 io.on('connection', (socket) => {
-    console.log("A user connected");
-})
+    console.log('A user connected');
+});
+
+mongoose
+    .connect('mongodb://localhost:27017/demo_chat', { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch((error) => handleError(error));
+
+mongoose.connection.on('error', (err) => {
+    logError(err);
+});
 
 var server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port);
